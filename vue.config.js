06fab21942e7ee0695493f69b23ce64740c9853b
leonardo bonilla <leonardo.bonilla@mundopacifico.cl>
2022-09-03 15:07:30 -0400

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: 'https://5g094irhc7.execute-api.us-east-1.amazonaws.com/',
+    allowedHosts: 'all',
 }
 })
